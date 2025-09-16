baa0e79cdaa3c6dcf1f3c208929afce916c1dfcf
Gustavo <gustavo.zuchini@etec.sp.gov.br>
2025-09-16 20:45:17 -0300

    Adicionando o VLibras

---
+++ js/vlibras.js
@@ -0,0 +1,5 @@
+new window.VLibras.Widget('https://vlibras.gov.br/app');
+
+document.getElementById("teste").addEventListener("click", () => {
+  alert("O VLibras já está rodando no seu site!");
+});
